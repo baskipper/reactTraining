0196dbf6ab7e8f55ec9c3015746b95c79cf646c0
bas0017 <bas0017@tigermail.auburn.edu>
2017-12-04 18:08:17 -0600

    Added PokeList component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'whatwg-fetch';
+import PokeList from './components/PokeList';
+import {Col} from 'react-bootstrap/lib/';
 
 class App extends Component {
 
@@ -40,9 +42,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Col sm={8} md={10} smOffset={2} mdOffset={1}>
+          <PokeList listOfPokemon={this.state.pokemon} />
+        </Col>
       </div>
     );
   }
