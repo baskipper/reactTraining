e140a19dac45c15cc414b7e5e79e43cce048b889
bas0017 <bas0017@tigermail.auburn.edu>
2017-12-06 18:11:48 -0600

    Added number of items per page control

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'whatwg-fetch';
 import PokeList from './components/PokeList';
+import SelectItemsPerPageButtons from './components/SelectItemsPerPageButtons';
 import { Col, Pagination } from 'react-bootstrap/lib/';
 
 class App extends Component {
@@ -11,14 +12,16 @@ class App extends Component {
     super(props);
     this.state = {
       pokemon: [],
-      activePage: 0,
+      activePage: 1,
       limit: 50,
       offset: 0,
-      totalPages: 0
+      totalPages: 0,
+      count: 0
     };
 
     this.loadPokemon = this.loadPokemon.bind(this);
     this.handlePaginationSelect = this.handlePaginationSelect.bind(this);
+    this.handleLimitChange = this.handleLimitChange.bind(this);
   }
 
   loadPokemon(url){
@@ -46,9 +49,27 @@ class App extends Component {
   handlePaginationSelect(event){
     console.log(event);
     let offset = this.state.limit * event;
-    let test = `${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=${offset}`;
-    console.log(test);
     this.loadPokemon(`${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=${offset}`);
+    this.setState({
+      activePage: event
+    })
+  }
+
+  handleLimitChange(event){
+    console.log(this.state.limit);
+    console.log('event');
+    console.log(event);
+    console.log('event.target');
+    console.log(event.target);
+    console.log('event.target.innerHtml');
+    console.log(event.target.innerHtml);
+    this.setState({
+      limit: +event.target.innerHTML || this.state.count,
+      activePage: 1
+    }, () =>{
+
+      this.loadPokemon(`${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=0`);
+    })
   }
 
   render() {
@@ -58,6 +79,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <SelectItemsPerPageButtons options={[10,50,100,200]} selectedValue={this.state.limit} allValue={this.state.count} onOptionSelected={this.handleLimitChange}/>
         <Col sm={8} md={10} smOffset={2} mdOffset={1}>
           <PokeList listOfPokemon={this.state.pokemon} />
         </Col>
